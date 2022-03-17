3efda849c525218308abf67bd50af1b70ea811f6
Ahmed Omar <ahmedomar@hey.com>
2022-03-17 12:46:43 +0000

    added stylings and created navbar

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./NavMenu.scss";
 
 const Navmenu = () => {
-  return <div>Navmenu</div>;
+  return (
+    <ul className="navmenu">
+      <li className="navmenu__item">
+        <a href="#activities" className="navmenu__anchor">
+          Activities
+        </a>
+      </li>
+      <li className="navmenu__item">
+        <a href="#animals" className="navmenu__anchor">
+          Animals
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default Navmenu;
